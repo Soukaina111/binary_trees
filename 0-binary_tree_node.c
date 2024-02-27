1a271c70f27032636a6b46940fb87d987c8b9b2e
Soukaina111 <basmahajar@gmail.com>
2024-02-27 07:31:28 -0800

    BTT

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,22 +9,21 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nnode;
+	binary_tree_t *nnode;
 
-    nnode = malloc(sizeof(binary_tree_t));
+	nnode = malloc(sizeof(binary_tree_t));
 
     if (nnode)
     {
-        nnode->parent = parent;
-        nnode->n = value;
-        nnode->left = NULL;
-        nnode->right = NULL;
+	    nnode->parent = parent;
+	    nnode->n = value;
+	    nnode->left = NULL;
+	    nnode->right = NULL;
     }
     else
     {
-        return (NULL);
+	    return (NULL);
     }
-   
     return (nnode);
 }
 
