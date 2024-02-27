6f8ac483d679ca3b3dbd2e8bc30ec87d2c9e09c7
Soukaina111 <basmahajar@gmail.com>
2024-02-27 08:14:23 -0800

    BTT

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,10 +7,12 @@
  * If node is NULL, returns 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node) {
-    if (node == NULL)
-    {
-        return (0);
-    }
-
-    return (node->left == NULL && node->right == NULL) ? 1 : 0;
+if (node == NULL)
+{
+return (0);
+}
+else
+{
+return (node->left == NULL && node->right == NULL) ? 1 : 0;
+}
 }
