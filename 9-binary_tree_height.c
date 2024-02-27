0cb133bfd8d5c5ab611697e9ebc8cdd99f827877
Soukaina111 <basmahajar@gmail.com>
2024-02-27 08:27:21 -0800

    BTT

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,23 +7,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t gauche = 0;
-    size_t droit = 0;
+	size_t gauche = 0;
+	size_t droit = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        if (tree->left != NULL)
-            gauche = 1 + binary_tree_height(tree->left);
-        if (tree->right != NULL)
-            droit = 1 + binary_tree_height(tree->right);
-        if (gauche > droit)
-            return (gauche);
-        else
-            return (droit);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree->left != NULL)
+			gauche = 1 + binary_tree_height(tree->left);
+		if (tree->right != NULL)
+			droit = 1 + binary_tree_height(tree->right);
+		if (gauche > droit)
+			return (gauche);
+		else
+			return (droit);
+	}
 }
 
