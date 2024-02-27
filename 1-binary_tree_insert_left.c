c27fc53d611e5faeebc37d6a210365bb15099feb
Soukaina111 <basmahajar@gmail.com>
2024-02-27 07:54:43 -0800

    BTT

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,13 +10,13 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *nnode;
+    nnode = binary_tree_node(parent, value);
 
     if (parent == NULL)
     {
         return (NULL);
     }
 
-    nnode = binary_tree_node(parent, value);
     if (nnode == NULL)
     {
         return (NULL);
