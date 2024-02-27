b4a3fc8987a3698315329bc5fb1237ff4d4b520d
Soukaina111 <basmahajar@gmail.com>
2024-02-27 08:09:50 -0800

    BTT

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,10 +7,12 @@
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
-        return;
-
+	    return;
+    else
+    {
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
     free(tree);
+    }
 }
 
