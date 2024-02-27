db1ff3939df9ebcb55551968dc17569954caefab
Soukaina111 <basmahajar@gmail.com>
2024-02-27 06:55:43 -0800

    BTT

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,9 +11,11 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-		
+	else
+	{
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
+	}
 	
 }
