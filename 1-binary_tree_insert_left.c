b89ab659200130d1034ac42cb5a6067e78de3581
Soukaina111 <basmahajar@gmail.com>
2024-02-27 07:59:02 -0800

    BTT

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,27 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
+    
+binary_tree_t *nnode = malloc(sizeof(binary_tree_t));	
+if (parent == NULL)
     {
-        return (NULL);  /* Return NULL if parent is NULL */
+        return (NULL);
     }
-
-    binary_tree_t *nnode = malloc(sizeof(binary_tree_t));
     if (nnode == NULL)
     {
         return (NULL);
     }
-
     nnode->value = value;
     nnode->left = NULL;
     nnode->right = NULL;
-
     if (parent->left != NULL)
-    {
-        
+    { 
         nnode->left = parent->left;
     }
-
     parent->left = nnode;
     return (nnode);
 }
