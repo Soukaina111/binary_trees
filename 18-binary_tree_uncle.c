e6de0b43f541e04f1619d0951cd15624573b1b59
Soukaina111 <basmahajar@gmail.com>
2024-02-28 09:03:43 -0800

    13

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,11 +7,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (node == NULL || node->parent == NULL)
-{
-return (NULL);
-}
-if ( node->parent->parent == NULL)
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 {
 return (NULL);
 }
@@ -20,4 +16,3 @@ else
 return (node->parent == node->parent->parent->left ?
 node->parent->parent->right : node->parent->parent->left);
 }
-
