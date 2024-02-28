ba868f8883ac548f9eef39687c394e5ee6a959b0
Soukaina111 <basmahajar@gmail.com>
2024-02-28 08:12:18 -0800

    13

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,46 +1,42 @@
 #include "binary_trees.h"
-/**
- * binary_tree_haut - Calculates the height of a binary tree.
- * @tree: Pointer to the root node of the tree.
- * Return: Height of the binary tree.
- */
-int binary_tree_haut(const binary_tree_t *tree)
-{
-	int gauche = 0;
-	int droit = 0;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			gauche = tree->gauche ? 1 + binary_tree_haut(tree->left) : 1;
-			droit = tree->droit ? 1 + binary_tree_haut(tree->right) : 1;
-		}
-		return ((gauche > droit) ? gauche : droit);
-	}
-}
 /**
- * binary_tree_balance -this function Measures
- * the balance factor of a binary tree.
- * @tree: Pointer to the root node of the tree.
- *
- * Return: Balance factor of the binary tree.
+ * binary_tree_height_alt -this function Measures
+ * height of a binary tree for a balanced tree
+ * @tree: The tree to traverse
+ * Return: The height
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height_alt(const binary_tree_t *tree)
 {
-	int gauche = 0;
-        int droit = 0;
-        int BF = 0;
+    size_t height_left = 0;
+    size_t height_right = 0;
 
-if (tree != NULL)
+    if (tree == NULL)
+        return (0);
+
+    height_left = tree->left ? 1 + binary_tree_height_alt(tree->left) : 1;
+    height_right = tree->right ? 1 + binary_tree_height_alt(tree->right) : 1;
+
+    return (height_left > height_right ? height_left : height_right);
+}
+
+/**
+ * binary_tree_balance_alt -this function Measures
+ * balance factor of a binary tree
+ * @tree: The tree to traverse
+ * Return: Balanced factor
+ */
+int binary_tree_balance_alt(const binary_tree_t *tree)
 {
-gauche = (binary_tree_haut(tree->left));
-droit = (binary_tree_haut(tree->right));
-BF = gauche - droit;
+    int height_left = 0, height_right = 0, balance_factor = 0;
+
+    if (tree)
+    {
+        height_left = (int)binary_tree_height_alt(tree->left);
+        height_right = (int)binary_tree_height_alt(tree->right);
+        balance_factor = height_left - height_right;
+    }
+
+    return (balance_factor);
 }
-return (BF);
-}
+
