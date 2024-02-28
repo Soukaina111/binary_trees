246547e5c5f7195987887d6e18b326b1b097be11
Soukaina111 <basmahajar@gmail.com>
2024-02-28 07:42:01 -0800

    13

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,21 +5,23 @@
  *
  * Return: Height of the binary tree.
  */
-int binary_tree_height(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
-    }
+int binary_tree_height(const binary_tree_t *tree)
+{
+int gauche_height;
+int droit_height;
+int max_height;
 
-    int gauche_height;
-    int droit_height;
-    int max_height;
-
-    gauche_height = binary_tree_height(tree->left);
-    droit_height = binary_tree_height(tree->right);
-
-    max_height = gauche_height > droit_height ? gauche_height : droit_height;
-
-    return (1 + max_height);
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+gauche_height = binary_tree_height(tree->left);
+droit_height = binary_tree_height(tree->right);
+max_height = gauche_height > droit_height ? gauche_height : droit_height;
+return (1 + max_height);
+}
 }
 /**
  * binary_tree_balance -The function that  measures
@@ -27,16 +29,19 @@ int binary_tree_height(const binary_tree_t *tree) {
  * @tree: Pointer to the root node of the tree.
  * Return: Balance factor of the binary tree.
  */
-int binary_tree_balance(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
-    }
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int gauche_height;
+int droit_height;
 
-    int gauche_height;
-    int droit_height;
-
-    gauche_height = binary_tree_height(tree->left);
-    droit_height = binary_tree_height(tree->right);
-
-    return (gauche_height - droit_height);
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+gauche_height = binary_tree_height(tree->left);
+droit_height = binary_tree_height(tree->right);
+return (gauche_height - droit_height);
+}
 }
