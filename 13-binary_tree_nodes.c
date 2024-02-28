dcf47ce0d57a82e98a9ebedf598e392be6393288
Soukaina111 <basmahajar@gmail.com>
2024-02-28 08:49:34 -0800

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,20 +7,18 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t gauche_nodes;
-    size_t droit_nodes;
+size_t gauche_nodes;
+size_t droit_nodes;
 
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        gauche_nodes = binary_tree_nodes(tree->left);
-        droit_nodes = binary_tree_nodes(tree->right);
-
-        return (gauche_nodes + droit_nodes + 1);
-    }
-
-    return (0);
+if (tree->left != NULL || tree->right != NULL)
+{
+gauche_nodes = binary_tree_nodes(tree->left);
+droit_nodes = binary_tree_nodes(tree->right);
+return (gauche_nodes + droit_nodes + 1);
+}
+return (0);
 }
 
