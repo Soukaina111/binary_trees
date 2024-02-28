bd9131d47963076c8dd896a708e3e7a0c79cf37b
Soukaina111 <basmahajar@gmail.com>
2024-02-28 08:52:56 -0800

    13

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,22 +5,20 @@
  *
  * Return: 1 if the tree is full, 0 otherwise.
  */
-int binary_tree_is_full(const binary_tree_t *tree) {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-
-    if (tree->left != NULL && tree->right != NULL)
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
-
-    return (0);
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
+if (tree->left != NULL && tree->right != NULL)
+{
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
+return (0);
 }
 
