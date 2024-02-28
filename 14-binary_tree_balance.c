c0ea08c82486fb81335a580c24ffa2791798d79e
Soukaina111 <basmahajar@gmail.com>
2024-02-28 10:35:00 -0800

    BTTT

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,13 +19,13 @@ size_t binary_tree_height_alt(const binary_tree_t *tree)
 
     return ((height_left > height_right ? height_left : height_right));
 }
-
 /**
  * binary_tree_balance_alt -this function Measures
  * balance factor of a binary tree
  * @tree: The tree to traverse
  * Return: Balanced factor
  */
+
 int binary_tree_balance_alt(const binary_tree_t *tree)
 {
     int height_left = 0, height_right = 0, balance_factor = 0;
