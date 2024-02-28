d2c969afadb76f53d4a142dfd3fefec3c6e0af68
Soukaina111 <basmahajar@gmail.com>
2024-02-28 08:43:33 -0800

    13

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,16 +7,18 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t gauche_size;
-    size_t droit_size;
+size_t gauche_size;
+size_t droit_size;
 
-    if (tree == NULL) {
-        return (0);
-    }
-
-    gauche_size = binary_tree_size(tree->left);
-    droit_size = binary_tree_size(tree->right);
-
-    return (gauche_size + droit_size + 1);
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+gauche_size = binary_tree_size(tree->left);
+droit_size = binary_tree_size(tree->right);
+return (gauche_size + droit_size + 1);
+}
 }
 
