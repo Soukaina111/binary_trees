4d8b7d35fdd4c671c4ab61268226d8124b59612a
Soukaina111 <basmahajar@gmail.com>
2024-02-28 08:55:06 -0800

    13

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,15 +9,17 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+if (node == NULL || node->parent == NULL)
+return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
+if (node->parent->left == node)
+return (node->parent->right);
 
-    if (node->parent->right == node)
-        return (node->parent->left);
-
-    return (NULL);
+if (node->parent->right == node)
+return (node->parent->left);
+else
+{
+return (NULL);
+}
 }
 
